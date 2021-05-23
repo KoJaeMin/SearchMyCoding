779969a443c80aea6122ecd62bf10e57e11b0529
Jae Min Ko <jvc123456@khu.ac.kr>
2021-05-23 16:04:51 +0900

    연동

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 const port = 8000
 
-app.get('/',(req,res) =>{res.sendFile(__dirname+'/public/')});//insert main html file
+app.get('/',(req,res) =>{res.sendFile(__dirname+'/home/home.html')});//insert main html file
+app.get('/MBTI/index.html',(req,res)=>{res.sendFile(__dirname+'/MBTI/index.html');});
 app.post('/', (req, res)=>{res.send('Got a POST request');});
 app.put('/',(req,res)=>{res.send('Got a PUT request')});
 app.delete('/',(req,res)=>{res.send('Got a DELETE request')});
 
 app.listen(port,()=>{console.log(`Exaple app listening at http://localhost:${port}`)})
-
