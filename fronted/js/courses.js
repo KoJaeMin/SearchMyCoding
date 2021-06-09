4a3dcf2d94ae8e51cbe890f88c034f888b3d243c
2017103982 <rohsy0818@naver.com>
2021-06-09 21:46:15 +0900

    강좌 조회 강좌 크기 조정, 무료 강좌 보기 수정

--- fronted/js/courses.js
+++ fronted/js/courses.js
@@ -46,4 +46,35 @@ $(function() {
             )
         }
     });
+
+    $.getJSON('/inflean_data' , function(res) {
+        $(".free_inflearn .courses a").remove();
+        for(var i=0;i<res.length;i++){
+            if(res[i].price=='무료'){
+                var rate=parseInt(res[i].rating)/20;
+                $(".free_inflearn .courses").append(
+                    `<a class="course" href="${res[i].link}">
+                        <div>
+                            <!--강의 이미지-->
+                            <div class="course_image">
+                                <img src="${res[i].image}" style="width:100%;height:100%;object-fit:cover;">
+                            </div>
+                            <!--강의 제목-->
+                            <div class="course_title">
+                                ${res[i].title}
+                            </div>
+                            <!--강의 가격-->
+                            <div class="course_price">
+                                ${res[i].price}
+                            </div>
+                            <!--강의 평점-->
+                            <div class="course_rating">
+                                ★ ${rate}/5
+                            </div>
+                        </div>
+                    </a>`
+                )
+            }
+        }
+    });
 });
