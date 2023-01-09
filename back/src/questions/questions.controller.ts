6f3a4975d614f8ca8ebe07c114f7ee5671a4c70a
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-09 23:07:07 +0900

    feat : Question CRU 기능 구현

---
+++ back/src/questions/questions.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Question } from 'src/entities/questions.entity';
+import { QuestionDto } from '../dto/Question.dto';
+import { QuestionsService } from './questions.service';
+
+@Controller('questions')
+export class QuestionsController {
+    constructor(private readonly questionService : QuestionsService){}
+
+    @Get()
+    getAllQuestions(){
+        this.questionService.getAllQuestions()
+    }
+
+    @Get(':id')
+    async getOneQusesion(@Param("id") questionId : number) : Promise<Question>{
+        return await this.questionService.getOneQusesion(questionId);
+    }
+
+    @Post()
+    createQuestion(@Body() questionDto : QuestionDto){
+        return this.questionService.createQuestion(questionDto);
+    }
+
+    @Patch(':id')
+    patchQuestion(@Param('id') questionId : number,@Body() questionDto : QuestionDto){
+        return this.questionService.patchQuestion(questionId, questionDto);
+    }
+}
---
+++ back/src/questions/questions.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { QuestionDto } from '../dto/Question.dto';
+import { Question } from '../entities/questions.entity';
+
+@Injectable()
+export class QuestionsService {
+    constructor(
+        @InjectRepository(Question)
+        private questionsRepository : Repository<Question>,
+    ){}
+
+    getAllQuestions() : Promise<Question[]>{
+        return this.questionsRepository.find();
+    }
+
+    async getOneQusesion(questionId : number) : Promise<Question>{
+        const FoundQuestion : Question = await this.questionsRepository.findOne({id : questionId});
+        if(!FoundQuestion)
+            throw new NotFoundException(`Course with Id ${questionId} is not found.`);
+        return FoundQuestion;
+    }
+
+    async createQuestion(QuestionDto : QuestionDto) : Promise<void>{
+        const {typeId, degree, contents} = QuestionDto;
+        const newQuestion : Question = this.questionsRepository.create({
+            typeId : typeId,
+            degree : degree,
+            contents : contents
+        })
+        await this.questionsRepository.insert(newQuestion)
+    }
+
+    async patchQuestion(questionId :number, updateQuestionData : QuestionDto) : Promise<void>{
+        try{
+            this.getOneQusesion(questionId);
+        }catch(err){}
+        await this.questionsRepository.update({id : questionId},updateQuestionData);
+    }
+}
