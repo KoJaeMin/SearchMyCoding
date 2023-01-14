57f4f3f6f88b004ca61003f837f4b406565da6e7
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-15 01:55:55 +0900

    chore : console.log 부분 삭제

--- back/src/questions/questions.controller.ts
+++ back/src/questions/questions.controller.ts
@@ -25,7 +25,6 @@ export class QuestionsController {
 
     @Post('type')
     createQuestionType(@Body() questionTypeDto : QuestionTypeDto){
-        console.log(questionTypeDto)
         return this.questionService.createQuestionType(questionTypeDto);
     }
 
