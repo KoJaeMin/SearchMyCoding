3207d3c347a862f4ce1d5c3dcceb2d2be0c9c842
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-22 23:33:38 +0900

    feat : main.ts에 config 변수 적용

--- back/QuestionServer/src/main.ts
+++ back/QuestionServer/src/main.ts
@@ -1,17 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { SwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
   
   const document = SwaggerModule.createDocument(app, SwaggerConfig);
   SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3000);
+
+  await app.listen(configService.get('SERVER_PORT'));
 }
 bootstrap();
