7e26ef3f6c4311f8e09eeca4dc5589bb76b715f6
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-22 23:34:28 +0900

    fix : directory명으로 인한 오류 수정

--- back/QuestionServer/src/questiontypes/questiontypes.module.ts
+++ back/QuestionServer/src/questiontypes/questiontypes.module.ts
@@ -1,4 +1,4 @@
-import { QuestionType } from 'QServer/src/entities/questiontypes.entity';
+import { QuestionType } from 'src/entities/questiontypes.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { QuestionTypesController } from './questiontypes.controller';
