e8adc6771b8473e32c77e4f1c9d367999590d7c5
Jae Min Ko <jvc123456@khu.ac.kr>
2023-01-23 12:58:28 +0900

    refactor : configService 적용

--- back/QuestionServer/src/config/typeorm.config.ts
+++ back/QuestionServer/src/config/typeorm.config.ts
@@ -1,15 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import 'dotenv/config';
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { QuestionType } from 'src/entities/questiontypes.entity';
 import { Question } from 'src/entities/questions.entity';
 
+const config : ConfigService = new ConfigService();
+
 export const typeORMConfig : TypeOrmModuleOptions = {
         type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASE,
+        host: config.get<ConfigService>('DATABASE_HOST'),
+        port: config.get<ConfigService>('DATABASE_PORT'),
+        username: config.get<ConfigService>('DATABASE_USERNAME'),
+        password: config.get<ConfigService>('DATABASE_PASSWORD'),
+        database: config.get<ConfigService>('DATABASE_DATABASE'),
         entities: [Question, QuestionType],
         synchronize : false
 }
