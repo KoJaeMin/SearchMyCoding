4c4bbe7fa829e16cb17847c14f2e506990dcb3b3
Jae Min Ko <jvc123456@khu.ac.kr>
2023-03-05 23:01:53 +0900

    style : 정확한 type 명시

--- back/QuestionServer/src/config/typeorm.config.ts
+++ back/QuestionServer/src/config/typeorm.config.ts
@@ -8,22 +8,22 @@ const config : ConfigService = new ConfigService();
 
 export const typeORMConfig : TypeOrmModuleOptions = {
         type: 'postgres',
-        host: config.get<ConfigService>('DATABASE_HOST'),
-        port: config.get<ConfigService>('DATABASE_PORT'),
-        username: config.get<ConfigService>('DATABASE_USERNAME'),
-        password: config.get<ConfigService>('DATABASE_PASSWORD'),
-        database: config.get<ConfigService>('DATABASE_DATABASE'),
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_DATABASE'),
         entities: [Question, Answer],
         synchronize : false
 }
 
 export const testTypeORMConfig : TypeOrmModuleOptions = {
         type: 'postgres',
-        host: config.get<ConfigService>('TEST_DATABASE_HOST'),
-        port: config.get<ConfigService>('TEST_DATABASE_PORT'),
-        username: config.get<ConfigService>('TEST_DATABASE_USERNAME'),
-        password: config.get<ConfigService>('TEST_DATABASE_PASSWORD'),
-        database: config.get<ConfigService>('TEST_DATABASE_DATABASE'),
+        host: config.get<string>('TEST_DATABASE_HOST'),
+        port: config.get<number>('TEST_DATABASE_PORT'),
+        username: config.get<string>('TEST_DATABASE_USERNAME'),
+        password: config.get<string>('TEST_DATABASE_PASSWORD'),
+        database: config.get<string>('TEST_DATABASE_DATABASE'),
         entities: [Question, Answer],
         synchronize : true
 }
