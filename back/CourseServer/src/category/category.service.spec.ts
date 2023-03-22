90116da2b61b36e754a5a91a96646d1aad9ec8ab
Jae Min Ko <jvc123456@khu.ac.kr>
2023-03-22 23:20:12 +0900

    feat : category기능 test cod 작성 for unit test

--- back/CourseServer/src/category/category.service.spec.ts
+++ back/CourseServer/src/category/category.service.spec.ts
@@ -1,12 +1,15 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateCategoryDto } from 'src/dto/CreateCategory.dto';
+import { UpdateCategoryDto } from 'src/dto/UpdateCategory.dto';
 import { Category } from 'src/entities/category.entity';
 import { Repository } from 'typeorm';
 import { CategoryService } from './category.service';
 
 const mockCategoryRepository = () => ({
   find: jest.fn(),
-  findOne: jest.fn(),
+  findOneBy: jest.fn(),
   create: jest.fn(),
   update: jest.fn(),
   insert: jest.fn()
@@ -46,8 +49,93 @@ describe('CategoryService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('getAllCategory');
-  it.todo('getOneCategory');
-  it.todo('createCategory');
-  it.todo('patchCategory');
+  describe('getAllCategory',()=>{
+    it('should find all category', async ()=>{
+      categoryRepository.find.mockResolvedValue([]);
+
+      const result = await service.getAllCategory();
+      expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOneCategory',()=>{
+    const findName : string = 'web';
+    const findErrorName : string = 'app';
+    it('should find a category',async ()=>{
+      categoryRepository.findOneBy.mockResolvedValue(mockedCategory);
+
+      const result = await service.getOneCategory(findName);
+      expect(categoryRepository.findOneBy).toHaveBeenCalledTimes(1);
+
+      expect(result).toEqual(mockedCategory);
+    });
+
+    it("should return a NotFoundException", async () => {
+      try{
+        await service.getOneCategory(findErrorName);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+  });
+  
+  describe('createCategory',()=>{
+    const mockedCreateCategoryDto : CreateCategoryDto = {
+      name : 'web'
+    };
+    it("should create a category", async () => {
+      categoryRepository.find.mockResolvedValue([]);
+      const BeforeCreate = (await service.getAllCategory()).length;
+      expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+      
+      const result = await service.createCategory(mockedCreateCategoryDto);
+
+      categoryRepository.find.mockResolvedValue([mockedCategory]);
+      const AfterCreate = (await service.getAllCategory()).length;
+      expect(categoryRepository.find).toHaveBeenCalledTimes(2);
+
+      expect(AfterCreate).toEqual(BeforeCreate + 1);
+    });
+  });
+  describe('patchCategory',()=>{
+    const mockedFindName : string = 'web';
+    const mockedUpdateCategoryName : string = 'app';
+    const mockedErrorUpdateCategoryName : string = 'AI';
+    const mockedUpdateCategoryDto : UpdateCategoryDto = {
+      name : 'app'
+    }
+    const mockedUpdateCategory : Category = {
+      id : 1,
+      name : 'app'
+    };
+
+    it("should patch a category", async () => {
+      categoryRepository.findOneBy.mockResolvedValue(mockedCategory);
+      const BeforeUpdate = await service.getOneCategory(mockedFindName);
+      expect(categoryRepository.findOneBy).toHaveBeenCalledTimes(1);
+      
+      const result = await service.patchCategory(mockedFindName, mockedUpdateCategoryDto)
+
+      categoryRepository.findOneBy.mockResolvedValue(mockedUpdateCategory);
+      const AfterUpdate = await service.getOneCategory(mockedUpdateCategoryName);
+      expect(categoryRepository.findOneBy).toHaveBeenCalledTimes(3);
+
+      expect(BeforeUpdate.id).toEqual(AfterUpdate.id);
+      expect(AfterUpdate.id).toEqual(mockedUpdateCategory.id);
+      expect(AfterUpdate.name).toEqual(mockedUpdateCategory.name);
+    });
+    
+    it("should return a NotFoundException", async () => {
+      categoryRepository.findOneBy.mockResolvedValue(mockedCategory);
+      const BeforeUpdate = await service.getOneCategory(mockedFindName);
+      expect(categoryRepository.findOneBy).toHaveBeenCalledTimes(1);
+      try{
+        await service.patchCategory(mockedErrorUpdateCategoryName, mockedUpdateCategoryDto);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
