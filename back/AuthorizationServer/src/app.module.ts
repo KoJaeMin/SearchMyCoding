457d3e0bfb0cb60b589e456b453b0b8f963a4885
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-09 23:48:44 +0900

    feat : mongodb에 user schema 정의

--- back/AuthorizationServer/src/app.module.ts
+++ back/AuthorizationServer/src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseURI } from './config/mongoose.config';
+import { User, UserSchema } from './schemas/user.schema';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
     isGlobal: true,
     }),
-    MongooseModule.forRoot(mongooseURI)
+    MongooseModule.forRoot(mongooseURI),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UserModule,
   ],
   controllers: [],
   providers: [],
--- back/AuthorizationServer/src/config/mongoose.config.ts
+++ back/AuthorizationServer/src/config/mongoose.config.ts
@@ -4,8 +4,8 @@ import 'dotenv/config';
 const config : ConfigService = new ConfigService();
 
 const type : string = 'mongodb';
-const mongodb_host : string = config.get<string>('DATABASE_HOST');
-const mongodb_port : number = config.get<number>('DATABASE_PORT');
-const mongodb_db : string = config.get<string>('DATABASE_DATABASE');
+const mongodb_host : string = config.get<string>('MONGODB_DATABASE_HOST');
+const mongodb_port : number = config.get<number>('MONGODB_DATABASE_PORT');
+const mongodb_db : string = config.get<string>('MONGODB_DATABASE_DATABASE');
 
 export const mongooseURI : string = `${type}://${mongodb_host}:${mongodb_port}/${mongodb_db}`;
