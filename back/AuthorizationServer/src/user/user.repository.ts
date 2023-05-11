390fd857d3e09ca7722b2ccb9a3a200c59116f81
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-11 23:42:36 +0900

    feat : mongoose를 이용하여 repository 기능 추가

--- back/AuthorizationServer/src/user/user.repository.ts
+++ back/AuthorizationServer/src/user/user.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { CreateUserDto } from "src/dto/CreateUser.dto";
+import { UpdateUserDto } from "src/dto/UpdateUser.dto";
 import { User, UserDocument } from "src/schemas/user.schema";
-import { DataSource, Repository } from "typeorm";
 
 @Injectable()
 export class UserRepository{
@@ -14,5 +15,25 @@ export class UserRepository{
   async findOne(email : string, password : string): Promise<User> {
     const User : User = await this.userModel.findOne({email:email, password:password});
     return User;
+  }
+
+  async creaateOne(createUserDto : CreateUserDto) : Promise<void>{
+    const createdUser = new this.userModel(createUserDto);
+    await createdUser.save();
+  }
+
+  async updatePassword(updateUserDto : UpdateUserDto){
+    await this.userModel.findOneAndUpdate({
+      name : updateUserDto.name,
+      email : updateUserDto.email
+    },
+    {
+      password: updateUserDto.password
+    },
+    {
+      new: true,
+      upsert: true
+    }
+    );
   }
 }
