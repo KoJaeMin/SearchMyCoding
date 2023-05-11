024d527891e494020b3d22f519d1c9034af1b636
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-11 23:27:20 +0900

    feat : User에 대한 dto 추가

---
+++ back/AuthorizationServer/src/dto/CreateUser.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateUserDto{
+    @ApiProperty({
+        description : "이름"
+    })
+    @IsString()
+    readonly name : string;
+
+    @ApiProperty({
+        description : "전자 메일, 아이디"
+    })
+    @IsString()
+    readonly email : string;
+
+    @ApiProperty({
+        description : "비밀번호"
+    })
+    @IsString()
+    readonly password : string;
+}
