673dc1faafd6dffcd2f30d3c4c210edf3837234f
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-12 10:27:26 +0900

    feat : dto 변경 및 추가

--- back/AuthorizationServer/src/dto/GetUser.dto.ts
+++ back/AuthorizationServer/src/dto/GetUser.dto.ts
@@ -1,16 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto{
+export class GetUserDto{
     @ApiProperty({
-        description : "이름"
+        description : "전자 메일, 아이디"
     })
     @IsString()
-    readonly name : string;
+    @IsNotEmpty()
+    readonly email : string;
 
     @ApiProperty({
         description : "비밀번호"
     })
     @IsString()
+    @IsNotEmpty()
     readonly password : string;
 }
