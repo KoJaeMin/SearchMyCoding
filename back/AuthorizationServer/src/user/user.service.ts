fe109c04ae1cb197edabf7c1c83bc913c50f6f67
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-15 12:04:58 +0900

    feat : 비번 찾기 기능 추가

--- back/AuthorizationServer/src/user/user.service.ts
+++ back/AuthorizationServer/src/user/user.service.ts
@@ -26,11 +26,42 @@ export class UserService {
         return User;
     }
 
+    async getUserWithoutPassword(email : string, name : string) : Promise<User>{
+        if(!IsValidEmail(email))
+            throw new BadRequestException(`Bad Email Format`);
+        const User : User = await this.userRepository.findOneWithoutPassword(email, name);
+
+        if(!User)
+            throw new NotFoundException(`User does not exist or the password is incorrect.`);
+        return User;
+    }
+
+    async changeDefaultPassword(user : User) : Promise<string>{
+        const toHash : string = `${Date.now()}${salt}${user.password}`;
+        const hashedPassword : string = crypto.createHash(hash_algorithm).update(toHash).digest('hex').slice(0, 9);
+        const userInfo = {
+            email : user.email,
+            password : user.password
+        };
+        const updatePassword = {
+            name : user.name,
+            password : hashedPassword
+        };
+        //// user 비번 바꾸기
+        try{
+            await this.userRepository.updatePassword(userInfo, updatePassword);
+        }catch(err){
+            throw err;
+        }
+
+        return hashedPassword;
+    }
+
     async SignUp(createUserDto : CreateUserDto) : Promise<void>{
         if(!IsValidEmail(createUserDto.email))
             throw new BadRequestException(`Bad Email Format`);
-        const toHash = `${createUserDto.password}${salt}`;
-        const hashedPassword = crypto.createHash(hash_algorithm).update(toHash).digest('hex');
+        const toHash : string = `${createUserDto.password}${salt}`;
+        const hashedPassword : string = crypto.createHash(hash_algorithm).update(toHash).digest('hex');
         await this.userRepository.creaateOne({
             email: createUserDto.email,
             password : hashedPassword,
