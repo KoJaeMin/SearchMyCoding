23560963344f9af8c096596c84a040a540d652bd
Jae Min Ko <jvc123456@khu.ac.kr>
2023-05-22 00:32:08 +0900

    feat : compareHash 함수 생성

--- back/AuthorizationServer/src/utils/hash.ts
+++ back/AuthorizationServer/src/utils/hash.ts
@@ -5,3 +5,7 @@ export function createHashPassword(passeord : string, length : number = 32) : st
     const toHash : string = `${passeord}${salt}`;
     return crypto.createHash(hash_algorithm).update(passeord).digest('hex').slice(0, length);
 }
+
+export function compareHash(password : string, hashedPassword : string) : boolean {
+    return createHashPassword(password) === hashedPassword;
+}
