d5382b98b150b4c2ab8864d86cb8254726a43ee9
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-02 00:51:44 +0900

    feat : redis 연결 options 세팅

--- back/AuthorizationServer/src/config/session.config.ts
+++ back/AuthorizationServer/src/config/session.config.ts
@@ -1,8 +1,18 @@
 import session from "express-session";
 import 'dotenv/config';
 import { ConfigService } from "@nestjs/config";
+import * as RedisStore from "connect-redis";
+import { Redis } from "ioredis";
 
 const config : ConfigService = new ConfigService();
+
+const clientOptions = new Redis({
+    host : config.get<string>('REDIS_HOST'),
+    port : config.get<number>('REDIS_PORT'),
+    tls : {
+        timeout : 60
+    }
+});
 
 export const sessionOption : session.SessionOptions = {
     secret : config.get<string>('SESSION_SECRET'),
@@ -10,6 +20,10 @@ export const sessionOption : session.SessionOptions = {
     saveUninitialized : true,
     cookie : {
         httpOnly: true,
-        maxAge : 60000
-    }
+        maxAge : 60000 /// 단위는 ms
+    },
+    store : new (RedisStore(session))({
+        client : clientOptions,
+        ttl : 60 /// 단위 s
+    })
 }
