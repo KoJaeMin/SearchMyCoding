64d0504f54577e517593092a8c513eae7fd66a90
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-02 21:16:00 +0900

    feat : auth module init

--- back/AuthorizationServer/src/app.module.ts
+++ back/AuthorizationServer/src/app.module.ts
@@ -4,15 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseURI } from './config/mongoose.config';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
+      isGlobal: true,
     }),
     MongooseModule.forRoot(mongooseURI),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
