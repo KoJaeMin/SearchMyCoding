dc7389006572f55ee4d38e91789509a65dc1d865
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-05 23:38:59 +0900

    feat : local auth guard 작성

---
+++ back/AuthorizationServer/src/guards/local.auth.guard.ts
@@ -0,0 +1,15 @@
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard("local") {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const result : boolean = (await super.canActivate(context)) as boolean;
+        
+    if (result) {
+      const request = context.switchToHttp().getRequest();
+      await super.logIn(request);
+    }
+    return result;
+  }
+}
