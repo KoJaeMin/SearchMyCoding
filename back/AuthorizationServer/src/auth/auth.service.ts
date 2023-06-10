846e719ba2a5e2f80a1dc5b1e9bdd1d042d610e0
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-10 23:44:34 +0900

    feat : logIn 기능 추가

--- back/AuthorizationServer/src/auth/auth.service.ts
+++ back/AuthorizationServer/src/auth/auth.service.ts
@@ -1,16 +1,27 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { User } from 'src/schemas/user.schema';
+import { compareHash } from 'src/utils/hash';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly usersService: UserService) {}
-      async validateUser(id: string, password: string) : Promise<User> {
+    async validateUser(id: string, password: string) : Promise<User> {
         try{
             const user : User = await this.usersService.getUserWithPassword(id, password);
             return user;
         }catch(err){
             throw new BadRequestException("User doesn't exist or Enter wrong information");
         }
-      }
+    }
+
+    async logIn(id : string, password : string){
+        const user : User = await this.usersService.getUser(id);
+        if(!compareHash(password, user.password)){
+            throw new UnauthorizedException(`password is wrong`);
+        }
+        /// 비림 번호를 제외한 정보 반환
+        const {password : pw, ...result} = user;
+        return result;
+    }
 }
