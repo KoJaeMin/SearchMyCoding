d5a2dd4ade6891679f2fe763dc730523ce8aa238
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-16 14:48:40 +0900

    fix : redis 연결 에러 수정

--- back/AuthorizationServer/src/config/session.config.ts
+++ back/AuthorizationServer/src/config/session.config.ts
@@ -1,17 +1,20 @@
 import session from "express-session";
 import 'dotenv/config';
 import { ConfigService } from "@nestjs/config";
-import * as connectRedis from 'connect-redis';
+import connectRedis from 'connect-redis';
 import { Redis } from "ioredis";
 
 const config : ConfigService = new ConfigService();
-
-const RedisStore = connectRedis(session);
 
 const clientOptions = new Redis({
     host : config.get<string>('REDIS_HOST'),
     port : config.get<number>('REDIS_PORT')
 });
+
+const RedisStore = new connectRedis({
+    client : clientOptions,
+    ttl : 60
+});;
 
 export const sessionOption : session.SessionOptions = {
     secret : config.get<string>('SESSION_SECRET'),
@@ -21,8 +24,5 @@ export const sessionOption : session.SessionOptions = {
         httpOnly: true,
         maxAge : 60000 /// 단위는 ms
     },
-    store : new RedisStore({
-        client : clientOptions,
-        ttl : 60
-    })
+    store : RedisStore
 }
