7eae9213256ac199db1e33ae4f89b48df223c413
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-16 15:03:59 +0900

    feat : session middleware에 passport 기능 추가

--- back/AuthorizationServer/src/middlewares/session.middleware.ts
+++ back/AuthorizationServer/src/middlewares/session.middleware.ts
@@ -1,4 +1,10 @@
+import { INestApplication } from "@nestjs/common";
 import session from "express-session";
 import { sessionOption } from "src/config/session.config";
+import passport from 'passport';
 
-export const SessionMiddleware = session(sessionOption);
+export function SessionMiddleware(app: INestApplication){
+    app.use(session(sessionOption));
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
