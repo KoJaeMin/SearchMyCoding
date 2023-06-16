376253f61d492240a53ec54eac4c0783cf3f4dee
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-16 15:03:09 +0900

    feat : swagger middleware 추가

--- back/AuthorizationServer/src/config/swagger.config.ts
+++ back/AuthorizationServer/src/config/swagger.config.ts
@@ -1,7 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder } from '@nestjs/swagger';
+
+const config : ConfigService = new ConfigService();
 
 export const SwaggerConfig = new DocumentBuilder()
     .setTitle('Authorization API')
     .setDescription('Authorization API description')
     .setVersion('0.0.1')
     .build();
+
+export const SwaggerPath = config.get<string>('SWAGGER_PATH');
