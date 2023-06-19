f5a32686ceca7a330e4dded047f2483ab61bd530
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-19 23:01:45 +0900

    chore : swagger cookie auth 추가

--- back/AuthorizationServer/src/config/swagger.config.ts
+++ back/AuthorizationServer/src/config/swagger.config.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 const config : ConfigService = new ConfigService();
 
 export const SwaggerConfig = new DocumentBuilder()
+    .addCookieAuth('connect.sid')
     .setTitle('Authorization API')
     .setDescription('Authorization API description')
     .setVersion('0.0.1')
