4cf733d52c0391fdae1860c3a0ba42e6f6c2553a
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-22 23:13:19 +0900

    feat : 로그인 되어있는 지 확인

---
+++ back/AuthorizationServer/src/guards/logged-in.guard.ts
@@ -0,0 +1,12 @@
+// auth/logged-in.guard.ts
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return await request.isAuthenticated();
+  }
+}
