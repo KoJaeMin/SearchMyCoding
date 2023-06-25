2ef252249ab5fa0af1b80c6f9be0e2b34b334c27
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-25 15:35:13 +0900

    feat : schema 유연성 설정

--- back/AuthorizationServer/src/schemas/user.schema.ts
+++ back/AuthorizationServer/src/schemas/user.schema.ts
@@ -3,7 +3,9 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document
 
-@Schema()
+@Schema({
+  strict : false
+})
 export class User {
   @Prop({required : true})
   name : string;
