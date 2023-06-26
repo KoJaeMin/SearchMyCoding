99da40ce24485fabb9d13c5f15d6f0862c1a71c4
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-26 14:57:35 +0900

    feat : 로그인 상태에서 활동시 session 계속 유지

--- back/AuthorizationServer/src/guards/logged-in.guard.ts
+++ back/AuthorizationServer/src/guards/logged-in.guard.ts
@@ -6,6 +6,10 @@ import { Request } from 'express';
 export class LoggedInGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request : Request = context.switchToHttp().getRequest();
+    const minute : number = 60 * 1000;
+    const updateExpires : Date = new Date(request.session.cookie.expires.getTime() + minute);
+    request.session.cookie.expires = updateExpires;
+    request.sessionStore.set(request.sessionID, request.session);
     return request.isAuthenticated();
   }
 }
