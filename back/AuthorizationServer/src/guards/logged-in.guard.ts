251e753a568c06df73844d14241293da02c22591
Jae Min Ko <jvc123456@khu.ac.kr>
2023-06-27 13:38:21 +0900

    fix : 인증 되어있는 request에 한해서 session 시간 연장

--- back/AuthorizationServer/src/guards/logged-in.guard.ts
+++ back/AuthorizationServer/src/guards/logged-in.guard.ts
@@ -6,10 +6,13 @@ import { Request } from 'express';
 export class LoggedInGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request : Request = context.switchToHttp().getRequest();
-    const minute : number = 60 * 1000;
-    const updateExpires : Date = new Date(request.session.cookie.expires.getTime() + minute);
-    request.session.cookie.expires = updateExpires;
-    request.sessionStore.set(request.sessionID, request.session);
-    return request.isAuthenticated();
+    const result : boolean = request.isAuthenticated();
+    if(result){
+      const minute : number = 60 * 1000;
+      const updateExpires : Date = new Date((new Date().getTime()) + minute);
+      request.session.cookie.expires = updateExpires;
+      request.sessionStore.set(request.sessionID, request.session);
+    }
+    return result;
   }
 }
